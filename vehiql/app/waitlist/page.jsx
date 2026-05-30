ad700b53bedcf4e24ca9d8f94b2b4229132c6a4d
prit2006 <pritpastagiya2006@gmail.com>
2026-05-31 01:53:52 +0530

    Add iframe for Car Marketplace Waitlist on Waitlist page

--- vehiql/app/waitlist/page.jsx
+++ vehiql/app/waitlist/page.jsx
@@ -10,7 +10,9 @@ export default function WaitlistPage() {
         <title>Waitlist</title>
       </Head>
       <div className="mt-20 fixed w-full">
-        <div className="aspect-video w-full">Add iframe here</div>
+        <div className="aspect-video w-full">
+          <iframe style="width: 100%;" height="569" src="https://vehiql-waitlish.created.app" title="Car Marketplace Waitlist" frameBorder="0"></iframe>
+          </div>
       </div>
     </>
   );
